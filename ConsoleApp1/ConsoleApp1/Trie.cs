ad12c8738dc8d9c06e13904bf8bd72b479639489
Krishnan Mahadevan <krishnanmiyer@gmail.com>
2021-05-06 19:12:58 -0700

    Trie V2

--- ConsoleApp1/ConsoleApp1/Trie.cs
+++ ConsoleApp1/ConsoleApp1/Trie.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace ConsoleApp1
@@ -20,6 +21,7 @@ namespace ConsoleApp1
         public Trie() {
             _root = new TrieNode();
         }
+
 
         public bool Search(string word) {
             TrieNode current = _root;
@@ -72,5 +74,78 @@ namespace ConsoleApp1
             }
             InsertR(current, word, index + 1);
         }        
+    }
+
+
+
+    public class TrieNodeV2
+    {
+        public char Value { get; set; }
+
+        public TrieNodeV2[] Children { get; set; }
+
+        public bool IsComplete { get; set; }
+
+        public TrieNodeV2(char c)
+        {
+            Value = c;
+        }
+
+        public TrieNodeV2() { }
+
+    }
+
+    public class TrieV2
+    {
+        private TrieNodeV2 _root;
+
+        public TrieV2()
+        {
+            _root = new TrieNodeV2();
+        }
+
+        public bool Search(string word)
+        {
+            if (_root == null) return false;
+
+            TrieNodeV2 current = _root;
+
+            for(int i = 0; i < word.Length; i++)
+            {
+                for(int j = 0; j < current.Children.Length; j++)
+                {
+                    if (current.Children[j].Value == word[i])
+                    {
+                        current = current.Children[j];
+
+                        if (i == word.Length - 1 && current.IsComplete) return true;
+
+                        break;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private bool SearchR(string word)
+        {
+            return SearchR(_root, word, 0);
+        }
+
+        private bool SearchR(TrieNodeV2 node, string word, int index)
+        {
+            if (index == word.Length - 1) return node.IsComplete;
+
+            for (int j = 0; j < node.Children.Length; j++)
+            {
+                if (node.Children[j].Value == word[index])
+                {
+                    return SearchR(node.Children[j], word, index++);
+                }
+            }
+
+            return false;
+        }
     }
 }
